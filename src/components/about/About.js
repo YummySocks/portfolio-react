af16d896fff5934a64cc4b74719dbd77863d6387
Socks <patricksutcliffe123@gmail.com>
2021-11-27 14:54:14 -0500

    added real about me

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -6,11 +6,9 @@ function AboutContainer () {
         <div id="intro">
             <h1>About Me</h1>
             <p >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent semper feugiat nibh sed pulvinar proin gravida hendrerit lectus. 
-                Lectus proin nibh nisl condimentum id. Amet risus nullam eget felis eget. Morbi tincidunt ornare massa eget egestas. 
-                Etiam tempor orci eu lobortis elementum nibh tellus. Posuere morbi leo urna molestie at elementum eu. Pharetra pharetra massa massa ultricies mi quis hendrerit dolor magna. 
-                Sit amet mauris commodo quis imperdiet massa tincidunt nunc. Sit amet consectetur adipiscing elit ut aliquam. Sit amet volutpat consequat mauris nunc congue nisi. 
-                Vestibulum morbi blandit cursus risus at ultrices mi. Congue quisque egestas diam in arcu cursus. Tempor commodo ullamcorper a lacus vestibulum.
+            Full-Stack web developer Recently earned a certificate in full stack development from the
+Georgia Tech Bootcamp, with newly developed skills in JavaScript, CSS, React.js, and responsive web design. Known for being passionate about building web-apps. Most recent project was I delivered a MERN E-commerce website with a team of four. I’m very excited to leverage my skills in a real world team with a quality driven mindset.
+
             </p>
             <img id="me" src={me} alt="Picture of my face"></img>
         </div>
